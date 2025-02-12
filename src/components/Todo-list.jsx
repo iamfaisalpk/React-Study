3353b1c57dd26138eb90ec8de608f88051d9eff7
iamfaisalpk <iamfaizy123@gmail.com>
2025-02-12 15:53:44 +0530

    seconded

--- src/components/Todo-list.jsx
+++ src/components/Todo-list.jsx
@@ -2,39 +2,39 @@ import React, { useState } from "react";
 import "./Todo.css"
 
 const Suii= ()=> {
-  const [tasks, setTasks] = useState([]);
-  const [input, setInput] = useState("");
+    const [tasks, setTasks] = useState([]);
+    const [input, setInput] = useState("");
 
-  const addTask = () => {
+const addTask = () => {
     if (input.trim() !== "") {
-      setTasks([...tasks, input]);
-      setInput("");
+    setTasks([...tasks, input]);
+    setInput("");
     }
-  };
+};
 
-  const deleteTask = (index) => {
+    const deleteTask = (index) => {
     setTasks(tasks.filter((_, i) => i !== index));
-  };
+};
 
-  return (
+return (
     <div className="todo-container">
-      <h2>To-Do List</h2>
-      <div>
+    <h2>To-Do List</h2>
+    <div>
         <input
-          type="text"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          placeholder="Add a task..."
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Add a task..."
         />
         <button onClick={addTask}>Add</button>
-      </div>
-      <ul>
+    </div>
+    <ul>
         {tasks.map((task, index) => (
-          <li key={index}>
+        <li key={index}>
             {task} <button onClick={() => deleteTask(index)}>Delete</button>
-          </li>
+        </li>
         ))}
-      </ul>
+    </ul>
     </div>
 );
 }
